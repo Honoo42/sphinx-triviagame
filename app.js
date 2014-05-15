e67d6c5563357fec5e89cba64fc4dffe8676153f
Sean Manhertz <sean.manhertz@gmail.com>
2014-05-15 12:05:31 -0600

    Beta to add chat and multiplayer functionality to trivia game

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var http = require('http');
+var socketio = require ('socket.io');
+
+// var bootstrap = require('twitter-bootstrap');
+var port = process.env.PORT || 7563;
+var app = express();
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+var server = app.listen(port, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
+socketio.listen(server).on('connection', function (socket) {
+
+	socket.on('message', function (msg) {
+
+		console.log('Message Received: ', msg);
+
+		socket.broadcast.emit('message', msg);
+	});
+});
